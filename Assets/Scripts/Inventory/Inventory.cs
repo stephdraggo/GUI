3ad297523ee16c7b4823b372853585e42e1e6613
stephdraggo <61036246+stephdraggo@users.noreply.github.com>
2020-10-30 11:47:03 +1100

    start inventories

---
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GUI3.Inventories
+{
+    public class Inventory : MonoBehaviour
+    {
+        #region Variables
+        [Header("Inventory Variables")]
+        public List<Item> inventory = new List<Item>();
+        public Item selectedItem;
+
+        [Header("Display Variables")]
+        [SerializeField] private bool showInv = false;
+        private string sortType = "";
+
+#if UNITY_EDITOR
+        private Vector2 scr;
+        private Vector2 scrollPosition;
+#endif
+        #endregion
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+        #region Functions
+        #endregion
+
+#if UNITY_EDITOR
+        private void OnGUI()
+        {
+            scr.x = Screen.width / 16;
+            scr.y = Screen.height / 9;
+
+            if (showInv)
+            {
+                GUI.Box(new Rect(0, 0, Screen.width, Screen.height), ""); //box of size screen
+
+                string[] itemTypes = Enum.GetNames(typeof(ItemType)); //array of item type names
+                int itemTypeCount = itemTypes.Length; //number of item types
+
+                for (int i = 0; i < itemTypeCount; i++) //for every item type
+                {
+                    if (GUI.Button(new Rect((4 + i) * scr.x, 0, scr.x, 0.25f * scr.y), itemTypes[i])) //make button
+                    {
+                        sortType = itemTypes[i];
+                    }
+                }
+
+                Display();
+
+            }
+        }
+
+        private void Display()
+        {
+            if (sortType == "")
+            {
+                scrollPosition = GUI.BeginScrollView(new Rect(0, 0.25f * scr.y, 3.75f * scr.x, 8.5f * scr.y), scrollPosition, new Rect(0, 0, 0, inventory.Count * .25f * scr.y), false, true);
+
+                for (int i = 0; i < inventory.Count; i++)
+                {
+                    if (GUI.Button(new Rect(0.5f * scr.x, (1 + i) * 0.25f * scr.y, 3 * scr.x, 0.25f * scr.y), inventory[i].Name))
+                    {
+                        selectedItem = inventory[i];
+                    }
+                }
+                GUI.EndScrollView();
+            }
+            else
+            {
+                ItemType type = (ItemType)Enum.Parse(typeof(ItemType), sortType); //get enum from string
+
+                int slotCount = 0;
+
+                for (int i = 0; i < inventory.Count; i++)
+                {
+                    if (inventory[i].Type == type)
+                    {
+                        if (GUI.Button(new Rect(0.5f * scr.x, (1 + slotCount) * 0.25f * scr.y, 3 * scr.x, 0.25f * scr.y), inventory[i].Name))
+                        {
+                            selectedItem = inventory[i];
+                        }
+                        slotCount++;
+                    }
+                }
+            }
+        }
+    }
+#endif
+}
---
+++ Assets/Scripts/Inventory/Item.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GUI3.Inventories
+{
+    public class Item
+    {
+        #region Private Variables
+        private int id;
+        private string name;
+        private string description;
+        private int eValue;
+        private int amount;
+        private Texture2D icon;
+        private GameObject mesh;
+        private ItemType type;
+        private int effectAmount;
+        #endregion
+
+        #region Public Properties
+        public int ID
+        {
+            get => id;
+            set => id = value;
+        }
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public string Description
+        {
+            get => description;
+            set => description = value;
+        }
+        public int Value
+        {
+            get => eValue;
+            set => eValue = value;
+        }
+        public int Amount
+        {
+            get => amount;
+            set => amount = value;
+        }
+        public Texture2D Icon
+        {
+            get => icon;
+            set => icon = value;
+        }
+        public GameObject Mesh
+        {
+            get => mesh;
+            set => mesh = value;
+        }
+        public ItemType Type
+        {
+            get => type;
+            set => type = value;
+        }
+        /// <summary>Damage, Armour or Heal amount depending on item type.</summary>
+        public int EffectAmount
+        {
+            get => effectAmount;
+            set => effectAmount = value;
+        }
+        #endregion
+    }
+
+    public enum ItemType
+    {
+        Food,
+        Weapon,
+        Apparel,
+        Crafting,
+        Ingredients,
+        Potions,
+        Scrolls,
+        Quest,
+        Money,
+    }
+}
